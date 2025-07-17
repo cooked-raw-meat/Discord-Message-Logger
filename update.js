afbf532be6e0492c84edce1a8193d029d74f4b95
cooked raw meat <kakunnoyuutyuubu@gmail.com>
2025-07-17 21:02:18 +0900

    Update update.js

--- update.js
+++ update.js
@@ -2,5 +2,5 @@ const fs = require("fs-extra");
 const git = require("simple-git");
 git().clone("https://github.com/cooked-raw-meat/Discord-Message-Logger", "temp");
 function updatecheck() {if (!fs.existsSync("temp")) {setTimeout(updatecheck, 1000);}else{update();}}
-function update () {fs.removeSync("temp/config.json");fs.copy("temp","./");fs.removeSync("temp");}
+function update () {fs.removeSync("temp/config.json");fs.copySync("temp","./");fs.removeSync("temp");}
 updatecheck();
