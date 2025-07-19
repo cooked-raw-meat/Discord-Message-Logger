d03d6a7243284a509365a6b7bd0aa526defabf72
cooked raw meat <kakunnoyuutyuubu@gmail.com>
2025-07-19 12:43:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js")
-const fs = require("fs-extra");
-const http = require("http");
-const readLastLines = require('read-last-lines');
+const fs = require("fs-extra")
+const http = require("http")
+const readLastLines = require('read-last-lines')
 const { token } = require("./config.json")
 var { datapath } = require("./config.json")
-const { port } = require("./config.json");
+const { port } = require("./config.json")
 const client = new Client({intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMembers,],})
 if (!fs.existsSync(datapath)) {fs.mkdirSync(datapath);}
 if (!datapath.endsWith("/")) {datapath = datapath+"/"}
@@ -26,17 +26,17 @@ http.createServer((req, res) => {
 	if (!port > 1024) {console.log("Set the port to 1024 higher");return;}
 	if (req.method === "POST") {
 		var body = ""
-		req.on("data", chunk => {body += chunk;});
+		req.on("data", chunk => {body += chunk;})
 	    req.on("end", () => {
 			if (!fs.existsSync(datapath+body+".txt")) {
-				var guildlist = fs.readdirSync(datapath);
+				var guildlist = fs.readdirSync(datapath)
 				for (i = 0;i < guildlist.length;i++) {
-					guildlist[i] = guildlist[i].replace(".txt","");
+					guildlist[i] = guildlist[i].replace(".txt","")
 					guild = client.guilds.cache.get(guildlist[i]);guildlist[i] = guildlist[i] + "("+guild.name+")\n";}
-					res.end("GuildID List\n"+guildlist.join(""));return;
+					res.end("GuildID List\n"+guildlist.join(""))
 				}
 		    readLastLines.read(datapath+body+".txt", 50)
-		    .then((lines) => res.end(lines));
+		    .then((lines) => res.end(lines))
 	    });
 	}else{
 		res.end("")
