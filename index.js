081d6e4d1e8c250bdb1578ca2e3077b639c23bd8
cooked raw meat <kakunnoyuutyuubu@gmail.com>
2025-10-29 13:58:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js")
-const fs = require("fs-extra")
+//load module
 const http = require("http")
-const readLastLines = require('read-last-lines')
+const fs = require("fs")
+const {Client, Events, GatewayIntentBits, ChannelManager, GuildAuditLogsEntry} = require("discord.js")
+const client = new Client({intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMembers,],})
+
+//load config
 const { token } = require("./config.json")
 var { datapath } = require("./config.json")
 const { port } = require("./config.json")
-const client = new Client({intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMembers,],})
+
+//setup
 if (!fs.existsSync(datapath)) {fs.mkdirSync(datapath);}
 if (!datapath.endsWith("/")) {datapath = datapath+"/"}
-client.login(token)
-client.on(Events.ClientReady, c => {console.log(`${c.user.tag}`)})
 
-client.on('messageCreate', async message => {
-if (message.author.bot) {return}
-if (message.content.replace(" ","") === "") {return}
-var content = message.author.username + " : " + message.content
-content = content.replace(/\n/g, " ")
-content = content.replace(/\d{5}/g, "")
-console.log(message.guild.name + " >> " + content)
-const filepath = datapath + message.guildId + ".txt"
-fs.appendFile(filepath, content + "\n", function (err) {if (err) throw err})
+//ready messasge
+client.on("clientReady", () => {
+  console.log(client.user.tag + " ready")
 })
 
-http.createServer((req, res) => {
-	if (!port > 1024) {console.log("Set the port to 1024 higher");return;}
-	if (req.method === "POST") {
-		var body = ""
-		req.on("data", chunk => {body += chunk;})
-	    req.on("end", () => {
-			var guildlist = fs.readdirSync(datapath)
-				for (i = 0;i < guildlist.length;i++) {
-					guildlist[i] = guildlist[i].replace(".txt","")
-					if (!client.guilds.cache.has(guildlist[i])) {
-						guildlist.splice(i, 1)
-						guildlist[i] = guildlist[i].replace(".txt","")
-					}
-					guild = client.guilds.cache.get(guildlist[i]);guildlist[i] = guildlist[i] + "("+guild.name+")\n";
-				}
-				res.end("GuildID List\n"+guildlist.join(""))
-	    });
-	}else{
-		res.end("")
-	}
-}).listen(port, () => console.log("has http server started a "+port+"port"))
+//monitoring message
+client.on("messageCreate", message => {
+  console.log(message.guild.name + ">>"+ message.channel.name + ">>" + message.author.username + ":" + message.content.replace(/\n/g," "))
+  if (!fs.existsSync(datapath + message.guild)) {fs.mkdirSync(datapath + message.guild)}
+  fs.appendFileSync(datapath + message.guild + "/" + message.channel + ".txt",message.author.username + ":" + message.content.replace(/\n/g," ") + "\n")
+});
 
+//server
+const server = http.createServer((req, res) => {
+  res.writeHead(200,{"content-type": "text/plain"})
+  if (req.method === "GET") {
+    res.end(client.user.tag + "\nping:" + client.ws.ping + "\nhost:" + process.platform + "\narch:" + process.arch)
+  }else if (req.method === "POST") {
+    var post = ""
+    req.on("data", chunk => {post += chunk;})
+    req.on("end", () => {
+      if (post.includes(",")) {
+        var GuildID = ""
+        var ChannelID = ""
+        for (let i = 0; i < 19; i++) {GuildID = GuildID + post[i]}
+        for (let i = 20; i < 39; i++) {ChannelID = ChannelID + post[i]}
+        if (!fs.existsSync(datapath + GuildID + "/" + ChannelID + ".txt")) {
+          res.end("File Not Found")
+          return
+        }
+        res.end(fs.readFileSync(datapath + GuildID + "/" + ChannelID + ".txt"))
+      }else if(post = " ") {
+        var DataList = ""
+        const GuildList = fs.readdirSync(datapath)
+        for (let i = 0; i < GuildList.length; i++) {
+          const ChannelList = fs.readdirSync(datapath + "/" + GuildList[i])
+          DataList = DataList + GuildList[i] + "(" +client.guilds.cache.get(GuildList[i]).name + ")\n"
+          for (let i = 0; i < ChannelList.length; i++) {
+            DataList = DataList + i + "." + ChannelList[i].replace(".txt", "") + "(" +client.channels.cache.get(ChannelList[i].replace(".txt", "")).name + ")\n"
+          }
+        }
+        res.end(DataList)
+      }else{
+        res.end("Syntax Error")
+      }
+    })
+  }
+}).listen(port)
+
+//login
+client.login(token)
